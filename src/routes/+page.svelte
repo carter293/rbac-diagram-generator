571d85d8aff974e876aa749f2efbd6b073d7a707
Matthew Carter <matthewcarter@matthews-mbp.home>
2023-03-26 10:12:53 +0800

    adding to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,6 @@
 <script lang="ts">
   import RBACDiagram from "../lib/components/RBACDiagram.svelte";
   import ScrollButton from "../lib/components/ScrollButton.svelte";
-  import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
-
-  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <main>
